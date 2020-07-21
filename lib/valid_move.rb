92063fc8b7f1b661f1a77a0fd8c13488ff7b36a3
Isabelle Ferreira <isabelle.a.ferreira@gmail.com>
2020-07-21 17:35:02 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move? (board, index)
   position = index.to_i - 1
-  if !position_taken?(board, index) && position.between?(0,8)
+  if !position_taken?(board, index) && position.between?(0,9)
     true
   else
     false
