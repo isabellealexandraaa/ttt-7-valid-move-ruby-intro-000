80e07a6eb3876803b4d2ce39759c94c41e823963
Isabelle Ferreira <isabelle.a.ferreira@gmail.com>
2020-07-21 18:01:26 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,6 +7,14 @@ def valid_move? (board, index)
       return true
     end
   end
+
+def on_board(num)
+  if num.between? (0, 8) == true
+    return true
+  else
+    return false
+  end
+end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 if (position_taken? (board, index)) == false && (on_board(index) == true)
   return true
